6fbcda8fd1b0f97c1bf2d1f680e04da66bf2032d
cayuval <jubim1234@gmail.com>
2022-06-27 17:17:05 +0300

    done

--- data/products.js
+++ data/products.js
@@ -52,7 +52,56 @@ const products = [
         'Kamilla Hart',
         522,
         false
-    )
+    ),
+    new Product(
+        'https://cdn.pixabay.com/photo/2022/03/01/00/12/tree-7040298_960_720.jpg',
+        'FALL',
+        'Carla Uno',
+        205,
+        true
+    ),
+    new Product(
+        'https://cdn.pixabay.com/photo/2022/06/22/06/53/cabinet-7277181_960_720.jpg',
+        'Home',
+        'Kamilla Hart',
+        300,
+        true
+    ),
+    new Product(
+        'https://cdn.pixabay.com/photo/2020/11/22/19/19/louvre-5767708_960_720.jpg',
+        'Renaissance',
+        'Bruno Brave',
+        800,
+        false
+    ),
+    new Product(
+        'https://cdn.pixabay.com/photo/2021/11/22/16/45/sheeps-6816871_960_720.jpg',
+        'Our Friends',
+        'Yuval Ben Haim',
+        255,
+        true
+    ),
+    new Product(
+        'https://cdn.pixabay.com/photo/2021/10/19/20/20/waterfall-6724514_960_720.jpg',
+        'Waterfall',
+        'Yuval Ben Haim',
+        200,
+        true
+    ),
+    new Product(
+        'https://cdn.pixabay.com/photo/2022/02/08/20/57/animals-7002172_960_720.jpg',
+        'FREE',
+        'Carla Uno',
+        450,
+        false
+    ),
+    new Product(
+        'https://cdn.pixabay.com/photo/2022/05/22/17/22/building-7214070_960_720.jpg',
+        'City',
+        'Kamilla Hart',
+        347,
+        true
+    ),
 ]
 
 export {products}
--- js/app.js
+++ js/app.js
@@ -1,25 +1,27 @@
 import { products } from '../data/products.js'
 
-console.log(products);
-
-
-
+//when clicking search call function searchProduct
 document.getElementById('searchButton').addEventListener('click', searchProduct)
 
 function searchProduct() {
+    //recive user's searcing and trim it in case of typing unnecessary spaces
     const userSearch = document.getElementById('searchField').value.trim()
 
+    //create a New array with objects that answer the terms os Search - all in Lower case 
     const searchResult = products.filter(product => {
         return product.name.toLowerCase().includes(userSearch.toLowerCase())
     })
     console.log(searchResult);
+    //and then display the objects inside the array
     displayProducts(searchResult)
 }
 
+//how many pictures do I see every time I reDisplay The Product
 function getTotalProducts(productsData) {
     return `(${productsData.length})`
 }
 
+//function that return only one big string of html Tags of the pictures I want to see
 function getProductsTemplate(productsData) {
     let cardsHtml = ''
     let cardStockHtml = ''
@@ -30,8 +32,8 @@ function getProductsTemplate(productsData) {
             cardStockHtml = 'Not Available'
         }
         cardsHtml +=
-            `<div class="card" style="width: 18rem;">
-      <img width="171" height="162" src="${product.getImg()}" class="card-img-top" alt="${product.getName()}">
+            `<div class="card" style="width: 16rem;">
+      <img src="${product.getImg()}" class="card-img-top" alt="${product.getName()}">
       <div class="card-body">
       <h3 class="card-text">${product.name}</h3>
       <h4 class="card-text">${product.artist}</h4>
@@ -44,9 +46,13 @@ function getProductsTemplate(productsData) {
 }
 
 function displayProducts(productsData) {
+    //catch the main data Element
     const mainData = document.querySelector('.container')
+    //catch the span Element of number of of pictures 
     const totalProducts = document.getElementById('num-of-products')
+    //inject the wanted template from the function getProductTemplate to the main data
     mainData.innerHTML = getProductsTemplate(productsData)
+    //inject the number of total products to the span Element
     totalProducts.innerHTML = getTotalProducts(productsData)
 }
 displayProducts(products)
